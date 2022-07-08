a0c3fbc3e24b617ada0dff9cee99537670c8441d
Abhijeet Deshmukh <abhijeetdeshmukh.2765@gmail.com>
2022-07-08 14:31:27 +0530

    file upload

---
+++ app/src/main/java/com/vrundavan_technoking/application1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vrundavan_technoking.application1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
